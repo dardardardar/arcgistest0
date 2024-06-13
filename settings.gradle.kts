be13371d2dc3fe87f3703cc05410023acf062c82
Dar <fauzi@infotech.co.id>
2024-06-13 10:32:23 +0700

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "arcgistest0"
+include(":app")
+ 
